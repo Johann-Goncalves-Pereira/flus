929e222dd0bfc1cb546450d1658e31875ef9ac2c
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2021-09-15 13:56:07 -0300

    feat: components on app

--- app/screens/ModalScreen.tsx
+++ app/screens/ModalScreen.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import * as React from "react";
 import { Platform, StyleSheet } from "react-native";
+import { Text, View } from "../components/Themed";
+import EditScreenInfo from "../components/EditScreenInfo";
 
-import EditScreenInfo from "../../components/EditScreenInfo";
-import { Text, View } from "../../components/Themed";
 
 export default function ModalScreen() {
   return (
